ff3ae35a8cbdfc8c46b5fdd92cbca1206e89e1b6
mdesform-hue <mdes.form@gmail.com>
2025-09-23 15:57:23 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'Mdes-Task',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'Mdes-Task-Tracker'
 };
 
 export default function RootLayout({
